d094f951e0227f33f108db402f52e3e96b4c07ac
Oki Oki <ociran@gmail.com>
2018-07-05 14:37:09 +0300

    test

--- slim_codebase_v2/vendor/utill/dal/Helper.php
+++ slim_codebase_v2/vendor/utill/dal/Helper.php
@@ -12,6 +12,9 @@ final class Helper {
     
     
     public static function haveRecord($result = null) {
+          print_r( 'eeeeeeeeeee') ; 
+        print_r($result['resultSet'][0]['control']) ; 
+          print_r( 'xxxxxxxxxxxxx') ; 
         if(isset($result['resultSet'][0]['control'])) return true;
         return false;
     }
