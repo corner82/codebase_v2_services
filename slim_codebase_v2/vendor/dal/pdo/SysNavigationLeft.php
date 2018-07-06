4febc8b76845db656155df1fe9af14e34542f9d8
Oki Oki <ociran@gmail.com>
2018-07-06 14:52:35 +0300

    menu sıralama

--- slim_codebase_v2/vendor/dal/pdo/SysNavigationLeft.php
+++ slim_codebase_v2/vendor/dal/pdo/SysNavigationLeft.php
@@ -648,7 +648,7 @@ class SysNavigationLeft extends \DAL\DalSlim {
             ) AS xtable 
                 WHERE 
                     active =0 
-             order by collapse desc , parent , menu_name
+             order by z_index -- collapse desc , parent , menu_name
                                  ";           
             //ORDER BY a.parent, a.z_index
             $statement = $pdo->prepare($sql);
